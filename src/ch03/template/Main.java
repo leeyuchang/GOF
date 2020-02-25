948401ebd7db66487d9f58805f97949f396c1279
leeyuchang <leeyucha@gmail.com>
2020-02-25 14:17:27 +0900

    add template partten

--- src/ch03/template/Main.java
+++ src/ch03/template/Main.java
@@ -7,7 +7,7 @@ public class Main {
     AbstractDisplay d2 = new StringDisplay("Hello, world.");    // "Hello, world."を持ったStringDisplayのインスタンスを1個作る。
     AbstractDisplay d3 = new StringDisplay("こんにちは。");     // "こんにちは。"を持ったStringDisplayのインスタンスを1個作る。
     d1.display();                                               // d1,d2,d3とも、すべて同じAbstractDisplayのサブクラスのインスタンスだから、
-    d2.display();                                               // 継承したdisplayメソッドを呼び出すことができる。
+    d2.display();                                               // displayメソッドを呼び出すことができる。
     d3.display();                                               // 実際の動作は個々のクラスCharDisplayやStringDisplayで定まる。
   }
 }
