074f6ce6d2129455282705cdb47447627e2e1f46
leeyuchang <leeyucha@gmail.com>
2020-02-25 14:19:53 +0900

    add template partten

--- src/ch03/template/StringDisplay.java
+++ src/ch03/template/StringDisplay.java
@@ -15,14 +15,6 @@ public class StringDisplay implements AbstractDisplay {
     printLine();
   }
 
-  private void printLine() {
-    System.out.print("+");
-    for (int i = 0; i < width; i++) {
-      System.out.print("-");
-    }
-    System.out.println("+");
-  }
-
   @Override
   public void print() {
     System.out.println("|" + string + "|");
@@ -31,6 +23,14 @@ public class StringDisplay implements AbstractDisplay {
   @Override
   public void close() {
     printLine();
+  }
+
+  private void printLine() {
+    System.out.print("+");
+    for (int i = 0; i < width; i++) {
+      System.out.print("-");
+    }
+    System.out.println("+");
   }
 
 }
