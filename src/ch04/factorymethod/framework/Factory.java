83fb729926c9b6a6ae19447f3efae69eb5073268
leeyuchang <leeyucha@gmail.com>
2020-02-25 16:23:59 +0900

    little bit changes

--- src/ch04/factorymethod/framework/Factory.java
+++ src/ch04/factorymethod/framework/Factory.java
@@ -2,13 +2,14 @@ package ch04.factorymethod.framework;
 
 public interface Factory {
 
+  public Product createProduct(String owner);
+  
+  public void registerProduct(Product product);
+
   default public Product create(String owner) {
     Product p = createProduct(owner);
     registerProduct(p);
     return p;
   }
 
-  public Product createProduct(String owner);
-
-  public void registerProduct(Product product);
 }
